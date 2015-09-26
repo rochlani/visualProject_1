45c83107918fa304fcba80512245714e53cc98b2
Rochlani gul <rochlani@vsnl.com>
2015-09-26 17:26:47 +0530

    commit fom vsnlc.om

--- gittest1/Form1.cs
+++ gittest1/Form1.cs
@@ -20,7 +20,7 @@ namespace gittest1
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("second save");
-            MessageBox.Show("third save");
+            MessageBox.Show("third  save");
         }
     }
 }
